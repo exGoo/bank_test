8d292e1cc31b6c850f69690e51bad1d6d4822c44
Eugene Vovk <evgeniyvovk2009@yandex.ru>
2024-11-25 11:29:49 +0300

    Подключить модуль с эврике

--- history/src/main/java/com/bank/history/HistoryApplication.java
+++ history/src/main/java/com/bank/history/HistoryApplication.java
@@ -2,9 +2,11 @@ package com.bank.history;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
+@EnableEurekaClient
 @SpringBootApplication
 public class HistoryApplication {
     public static void main(String[] args) {
