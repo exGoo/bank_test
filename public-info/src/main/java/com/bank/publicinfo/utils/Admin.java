6bc36df10cde9f3f555b851725868e62c4e65cc1
artemioFingerini <artemisanx@gmail.com>
2024-11-28 16:30:27 +0300

    повтор

---
+++ public-info/src/main/java/com/bank/publicinfo/utils/Admin.java
@@ -0,0 +1,15 @@
+package com.bank.publicinfo.utils;
+
+import lombok.Data;
+import org.springframework.stereotype.Component;
+
+@Data
+@Component
+public class Admin {
+    private final String username = "admin";
+
+    private final String password = "54321";
+
+    private final String role = "ROLE_ADMIN";
+
+}
