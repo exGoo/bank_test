ab2b9415309bd00dae1bb38eb5b2e8a44ac0a076
Vladislav Guliaev <nascentesxxx@gmail.com>
2024-11-28 17:06:04 +0400

    Fix

--- antifraud/src/main/java/com/bank/antifraud/repository/AuditRepository.java
+++ antifraud/src/main/java/com/bank/antifraud/repository/AuditRepository.java
@@ -2,9 +2,11 @@ package com.bank.antifraud.repository;
 
 import com.bank.antifraud.model.Audit;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface AuditRepository extends JpaRepository<Audit, Long> {
 
     Optional<Audit> findTopByNewEntityJson(String entityJson);
--- antifraud/src/main/java/com/bank/antifraud/repository/SuspiciousPhoneTransfersRepository.java
+++ antifraud/src/main/java/com/bank/antifraud/repository/SuspiciousPhoneTransfersRepository.java
@@ -2,6 +2,8 @@ package com.bank.antifraud.repository;
 
 import com.bank.antifraud.model.SuspiciousPhoneTransfers;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface SuspiciousPhoneTransfersRepository extends JpaRepository<SuspiciousPhoneTransfers, Long> {
 }
